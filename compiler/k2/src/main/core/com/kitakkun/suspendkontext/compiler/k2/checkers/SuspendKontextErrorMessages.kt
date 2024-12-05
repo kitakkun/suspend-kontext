88b59f3e87c94d9184db0a18a840f3f9aa7abb4b
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-12-05 22:15:04 +0900

    Add checker for custom context definitions

--- compiler/k2/src/main/core/com/kitakkun/suspendkontext/compiler/k2/checkers/SuspendKontextErrorMessages.kt
+++ compiler/k2/src/main/core/com/kitakkun/suspendkontext/compiler/k2/checkers/SuspendKontextErrorMessages.kt
@@ -3,11 +3,15 @@ package com.kitakkun.suspendkontext.compiler.k2.checkers
 import org.jetbrains.kotlin.diagnostics.KtDiagnosticFactoryToRendererMap
 import org.jetbrains.kotlin.diagnostics.rendering.BaseDiagnosticRendererFactory
 
-object SuspendKontextErrorMessages: BaseDiagnosticRendererFactory() {
+object SuspendKontextErrorMessages : BaseDiagnosticRendererFactory() {
     override val MAP: KtDiagnosticFactoryToRendererMap = KtDiagnosticFactoryToRendererMap("suspend-kontext").apply {
         put(
             factory = SuspendKontextErrors.NON_SUSPEND_FUNCTION,
             message = "Functions annotated with suspend-kontext annotations must be suspend."
+        )
+        put(
+            factory = SuspendKontextErrors.NON_DISPATCHER_CLASS,
+            message = "the dispatcher for CustomContext must be a super class of kotlinx.coroutines.CoroutineDispatcher.",
         )
     }
 }
--- compiler/k2/src/main/core/com/kitakkun/suspendkontext/compiler/k2/extension/SuspendKontextFirCheckersExtension.kt
+++ compiler/k2/src/main/core/com/kitakkun/suspendkontext/compiler/k2/extension/SuspendKontextFirCheckersExtension.kt
@@ -2,15 +2,21 @@ package com.kitakkun.suspendkontext.compiler.k2.extension
 
 import com.kitakkun.suspendkontext.compiler.k2.SuspendKontextPredicate
 import com.kitakkun.suspendkontext.compiler.k2.checkers.AnnotationUseSiteChecker
+import com.kitakkun.suspendkontext.compiler.k2.checkers.CustomContextDefinitionChecker
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.analysis.checkers.declaration.DeclarationCheckers
 import org.jetbrains.kotlin.fir.analysis.checkers.declaration.FirFunctionChecker
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.ExpressionCheckers
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirAnnotationCallChecker
 import org.jetbrains.kotlin.fir.analysis.extensions.FirAdditionalCheckersExtension
 import org.jetbrains.kotlin.fir.extensions.FirDeclarationPredicateRegistrar
 
 class SuspendKontextFirCheckersExtension(session: FirSession) : FirAdditionalCheckersExtension(session) {
     override val declarationCheckers: DeclarationCheckers = object : DeclarationCheckers() {
         override val functionCheckers: Set<FirFunctionChecker> = setOf(AnnotationUseSiteChecker)
+    }
+    override val expressionCheckers: ExpressionCheckers = object : ExpressionCheckers() {
+        override val annotationCallCheckers: Set<FirAnnotationCallChecker> = setOf(CustomContextDefinitionChecker)
     }
 
     override fun FirDeclarationPredicateRegistrar.registerPredicates() {
